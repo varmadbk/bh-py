d927f97f26bbce4028e96b9298fe6096d6d04173
redashu <ashutoshh@linux.com>
2024-05-28 19:45:09 +0530

    check python

---
+++ bh-pyt.py
@@ -0,0 +1,10 @@
+import time
+
+for i in range(15):
+    print("Hello all , welcome to python..!!")
+    time.sleep(3)
+    print("Welcome to BMO..")
+    time.sleep(2)
+    print("Welcome to Containers ..!!")
+    print("______________________")
+    time.sleep(3)
